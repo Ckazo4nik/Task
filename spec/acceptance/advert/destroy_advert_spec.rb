3e17723aded2a055538b1b4bc6a22ba3f669063e
Orest Kostiuk <xxmolekula@gmail.com>
2017-11-01 19:49:29 +0200

    test delete advert

---
+++ spec/acceptance/advert/destroy_advert_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature 'видалення оголошення', %q{
+  Користувач може видалити своє оголошення
+} do
+  given!(:user) { create(:user) }
+  given!(:advert) { create(:advert,user_id: user.id)}
+  scenario 'Користувач удаляє своє оголошення' do
+    sign_in(user)
+    visit root_path
+    click_on 'видалити'
+    expect(page).to_not have_content advert.title
+    end
+  scenario 'не видно силку видалити' do
+    visit root_path
+    expect(page).to_not have_content 'видалити'
+  end
+end
